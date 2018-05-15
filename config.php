d10273b7956217a0936d2723f4db62d4ea2b76ce
yoyok173 <saied.five@gmail.com>
2018-05-15 19:50:21 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
-$channelAccessToken = 'CHANNEL ACCESS TOKEN';
+$channelAccessToken = 'g/f8PzOjZdBVMFR5GGELA5hxWg27jgzGofBYr/t7j3zfOW4GFr42vzOPGs296joyvpx7YSthAGkCO5iDUvdssFu4jJtW1bfENLwIXWwDQtrujdBw6YSt7vvJZk4qoyfOY0/NGSKTz2nUd3kuPatmUAdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = 'CHANNEL SECRET';
+$channelSecret = '503c9340cc0a0691641cd183d9965894';
